128fa1878860d8c16d0b811c39c1c72367306736
Brucce <9596701+BrucceNeves@users.noreply.github.com>
2023-05-05 22:34:54 -0400

    included LPHN, LLGC, GFHF
    added more algorithms

--- graphtlp/__init__.py
+++ graphtlp/__init__.py
@@ -1,39 +1,94 @@
 def __transductiveLabelPropagation__(iterations, convergenceThreshold, network, model):
   from numpy import abs, array, zeros
-  edges = network.edges
   matrix = network.matrix
-  vector_size = network.vector_size
-  count = 0
   node2id = {}
   matrix_f = []
-  for index, y in matrix.values:
-    node2id[index] = count
-    count += 1
-    matrix_f.append(zeros(vector_size) if y is None else y)
+  for id, (index, y) in enumerate(aux.matrix.values):
+    node2id[index] = id
+    matrix_f.append(zeros(network.vector_size) if y is None else y)
   matrix_f = array(matrix_f)
-  model.calculete_degree(edges, node2id)
+  model.calculete_degree(network.edges, network.degree, node2id)
   for i in range(iterations):
     new_f = model.labelPropagation(matrix, matrix_f)
     convergence_value = abs(matrix_f - new_f).sum()
     matrix_f = array(new_f)
     if convergence_value < convergenceThreshold:
       break
-  return zip(list(node2id.keys()), matrix_f)
+  return i+1, zip(list(node2id.keys()), matrix_f)
+
+class GFHF:
+  def calculete_degree(self, edges, degree, node2id):
+    self.relations = {}
+    for n1, n2, weight, _ in edges.values:
+      if n1 not in self.relations:
+        self.relations[n1] = {'adjs': [], 'weight': [], 'degree': 0}
+      if n2 not in self.relations:
+        self.relations[n2] = {'adjs': [], 'weight': [], 'degree': 0}
+      self.relations[n1]['adjs'].append(node2id[n2])
+      self.relations[n2]['adjs'].append(node2id[n1])
+      self.relations[n1]['weight'].append([weight])
+      self.relations[n2]['weight'].append([weight])
+      self.relations[n1]['degree'] += weight
+      self.relations[n2]['degree'] += weight
+
+  def labelPropagation(self, matrix, matrix_f):
+    new_f = []
+    for node, y in matrix.values:
+      if y is not None:
+        new_f.append(y)
+        continue
+      n = self.relations[node]
+      f = (matrix_f[n['adjs']] * n['weight']).sum(axis=0) / n['degree']
+      new_f.append(f)
+    return new_f
+  
+  def train(self, iterations:int, convergenceThreshold:float, network): return __transductiveLabelPropagation__(iterations, convergenceThreshold, network, self)
+
+class LLGC:
+  def __init__(self, mi:float):
+    self.mi = mi
+
+  def calculete_degree(self, edges, degree, node2id):
+    from numpy import sqrt
+    degree = sqrt(degree.groupby('n1').sum(numeric_only=True))
+    degree = degree.to_dict(orient="index")
+    self.relations = {}
+    for n1, n2, weight, _ in edges.values:
+      rdegree = weight/(degree[n1]['weight'] * degree[n2]['weight'])
+      if n1 not in self.relations:
+        self.relations[n1] = {'adjs': [], 'weight': []}
+      if n2 not in self.relations:
+        self.relations[n2] = {'adjs': [], 'weight': []}
+      self.relations[n1]['adjs'].append(node2id[n2])
+      self.relations[n2]['adjs'].append(node2id[n1])
+      self.relations[n1]['weight'].append([rdegree])
+      self.relations[n2]['weight'].append([rdegree])
+
+  def labelPropagation(self, matrix, matrix_f):
+    new_f = []
+    for node, y in matrix.values:
+      if y is not None and self.mi == 1:
+        new_f.append(y)
+        continue
+      n = self.relations[node]
+      f = (matrix_f[n['adjs']] * n['weight']).sum(axis=0)
+      if y is not None:
+        f += (y * self.mi)
+      new_f.append(f)
+    return new_f
+  
+  def train(self, iterations:int, convergenceThreshold:float, network): return __transductiveLabelPropagation__(iterations, convergenceThreshold, network, self)
 
 class GNetMine:
   def __init__(self, mi:float, weight_relations:dict={}):
     self.mi = mi
     self.weight_relations = weight_relations
 
-  def calculete_degree(self, edges, node2id):
+  def calculete_degree(self, edges, degree, node2id):
     from numpy import sqrt
-    from pandas import concat
     if len(self.weight_relations) == 0:
       self.weight_relations = {r_type_id: 1 for r_type_id in edges['r_type'].unique()}
     self.weight_relations = {r_type: value/len(self.weight_relations) for r_type, value in self.weight_relations.items()}
-    degree = edges.copy()
-    degree.columns = ['n2', 'n1', 'weight', 'r_type']
-    degree = concat([edges, degree], ignore_index=True)
     degree = sqrt(degree.groupby(['r_type', 'n1']).sum(numeric_only=True))
     degree = degree.to_dict(orient="index")
     self.relations = {}
@@ -58,6 +113,38 @@ class GNetMine:
       f = (matrix_f[n['adjs']] * n['weight']).sum(axis=0)
       if y is not None:
         f += (y * self.mi)
+      new_f.append(f)
+    return new_f
+  
+  def train(self, iterations:int, convergenceThreshold:float, network): return __transductiveLabelPropagation__(iterations, convergenceThreshold, network, self)
+
+class LPHN:
+  def calculete_degree(self, edges, degree, node2id):
+    from numpy import sqrt
+    degree = sqrt(degree.groupby(['r_type', 'n1']).sum(numeric_only=True))
+    degree = degree.to_dict(orient="index")
+    self.relations = {}
+    for n1, n2, weight, rtype in edges.values:
+      rdegree = weight/(degree[(rtype, n1)]['weight'] * degree[(rtype, n2)]['weight'])
+      if n1 not in self.relations:
+        self.relations[n1] = {'adjs': [], 'weight': [], 'degree': 0}
+      if n2 not in self.relations:
+        self.relations[n2] = {'adjs': [], 'weight': [], 'degree': 0}
+      self.relations[n1]['adjs'].append(node2id[n2])
+      self.relations[n2]['adjs'].append(node2id[n1])
+      self.relations[n1]['weight'].append([rdegree])
+      self.relations[n2]['weight'].append([rdegree])
+      self.relations[n1]['degree'] += weight
+      self.relations[n2]['degree'] += weight
+
+  def labelPropagation(self, matrix, matrix_f):
+    new_f = []
+    for node, y in matrix.values:
+      if y is not None:
+        new_f.append(y)
+        continue
+      n = self.relations[node]
+      f = (matrix_f[n['adjs']] * n['weight']).sum(axis=0) / n['degree']
       new_f.append(f)
     return new_f
   
--- graphtlp/network.py
+++ graphtlp/network.py
@@ -1,12 +1,15 @@
 class tlp_network:
   def __init__(self, network, labels):
-    from pandas import DataFrame
+    from pandas import DataFrame, concat
     self.edges = DataFrame([{'n1': n1, 'n2': n2, 'r_type': self.__node2relation(n1, n2)} | w for n1, n2, w in network.edges(data=True)])[['n1', 'n2', 'weight', 'r_type']]
     self.vector_size = len(labels['y'][0])
     x = [{'node': v, 'y': None} for v in network.nodes]
     self.matrix = DataFrame(x).set_index('node')
     self.matrix.loc[labels.index, 'y'] = labels['y']
     self.matrix.reset_index(inplace=True)
+    degree = self.edges.copy()
+    degree.columns = ['n2', 'n1', 'weight', 'r_type']
+    self.degree = concat([self.edges, degree], ignore_index=True)
 
   def __node2relation(self, n1, n2):
     x = [x.split(':')[-1].lower() for x in [n1, n2]]
